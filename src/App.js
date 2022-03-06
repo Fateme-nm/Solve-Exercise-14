19c6aefa961e954240efdb437220e629668143e6
fatemeh <fatemehnm1378@gmail.com>
2022-03-06 20:30:29 +0330

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Modal from './components/Modal/Modal';
+import './App.css'
+import Votingapp from './components/VotingApp/VotingApp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visiable : false
+    }
+  }
+
+  VisiableHandler = () => {
+    this.setState({visiable : !(this.state.visiable)})
+  }
+
+  render() {
+    return (
+      <div className='main'>
+        <button className='modalBtn' onClick={this.VisiableHandler}>click</button>
+        <Modal visiable ={this.state.visiable} closeHandler = {this.VisiableHandler}/>
+        <Votingapp />
+      </div>
+    );
+  }
 }
 
 export default App;
